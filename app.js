349cef87515c0efc37fd511b9569121bfcba0af9
alexandre <aobellianne@phoceis.asia>
2016-11-22 16:09:55 +0800

    hello world

--- app.js
+++ app.js
@@ -1,27 +1,93 @@
-/**
- * Created by alexandre on 21/11/2016.
- */
-var restify = require('restify');
-var builder = require('botbuilder');
+///**
+// * Created by alexandre on 21/11/2016.
+// */
+//var restify = require('restify');
+//var builder = require('botbuilder');
+//
+////var server = restify.createServer();
+////server.listen(process.env.PORT || 3000, function (){
+////    console.log('%s listening to %s', server.name, server.url);
+////});
+//
+////var connector = new builder.ChatConnector({
+////    appId: '1acda086-2dbe-4d12-8690-a4b19a8985f2',
+////    appPassword: 'PJN3jkngKiucpUMgzaxkmFz'
+////});
+//
+//var connector = new builder.ConsoleConnector().listen();
+//
+//
+//
+////server.post('/api/message', connector.listen());
+//
+//
+//
+//var bot = new builder.UniversalBot(connector);
+//var intents = new builder.IntentDialog();
+//
+//intents.matches(/^change name/i, [
+//    function (session) {
+//        session.beginDialog('/profile');
+//    },
+//    function (session, results) {
+//        session.send('Ok... Changed your name to %s', session.userData.name);
+//    }
+//]);
+//
+//intents.onDefault([
+//    function (session, args, next) {
+//        if (!session.userData.name) {
+//            session.beginDialog('/profile');
+//        } else {
+//            next();
+//        }
+//    },
+//    function (session, results) {
+//        session.send('Hello %s!', session.userData.name);
+//    }
+//]);
+//
+//bot.dialog('/profile', [
+//    function (session) {
+//        builder.Prompts.text(session, 'Hi! What is your name?');
+//    },
+//    function (session, results) {
+//        session.userData.name = results.response;
+//        session.endDialog();
+//    }
+//]);
+//
+////bot.dialog('/', function(session) {
+////    session.send('Hello World');
+////});
 
+var builder = require('botbuilder');
+var restify = require('restify');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function (){
+server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
+// Create chat bot
 var connector = new builder.ChatConnector({
     appId: '1acda086-2dbe-4d12-8690-a4b19a8985f2',
     appPassword: 'PJN3jkngKiucpUMgzaxkmFz'
 });
-
-
-
-server.post('/api/message', connector.listen());
-
-
-
 var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
 var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
 
 intents.matches(/^change name/i, [
     function (session) {
@@ -54,7 +120,3 @@ bot.dialog('/profile', [
         session.endDialog();
     }
 ]);
-
-//bot.dialog('/', function(session) {
-//    session.send('Hello World');
-//});
