3d64f05252efa0ff42e565a6f57cb2b2112006ee
alexandre <aobellianne@phoceis.asia>
2016-11-22 14:34:01 +0800

    hello world

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ var http = require('http');
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World\n");
+    response.end("Hello Guys, how are you ?\n");
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
