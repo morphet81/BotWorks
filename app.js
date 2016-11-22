8b0f20ac8e03d8f8ce7bb99a846d308a33eb89a3
alexandre <aobellianne@phoceis.asia>
2016-11-22 14:56:50 +0800

    hello world

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
 /**
  * Created by alexandre on 21/11/2016.
  */
-//var restify = require('restify');
-//var builder = require('botbuilder');
-//
-//var server = restify.createServer();
-//server.listen(process.env.PORT || 3000, function (){
-//    console.log('%s listening to %s', server.name, server.url);
-//});
-//
-//var connector = new builder.ChatConnector({
-//    appId: '1acda086-2dbe-4d12-8690-a4b19a8985f2',
-//    appPassword: 'PJN3jkngKiucpUMgzaxkmFz'
-//});
-//
-//var bot = new builder.UniversalBot(connector);
-//
-//server.post('/api/message', connector.listen());
-//
-//bot.dialog('/', function(session) {
-//    session.send('Hello World');
-//});
-
-// Load the http module to create an http server.
-var http = require('http');
+var restify = require('restify');
 var builder = require('botbuilder');
+
+var server = restify.createServer();
+server.listen(process.env.PORT || 3000, function (){
+    console.log('%s listening to %s', server.name, server.url);
+});
 
 var connector = new builder.ChatConnector({
     appId: '1acda086-2dbe-4d12-8690-a4b19a8985f2',
@@ -33,15 +16,8 @@ var connector = new builder.ChatConnector({
 
 var bot = new builder.UniversalBot(connector);
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(connector.listen());
-
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(process.env.PORT || 3000);
+server.post('/api/message', connector.listen());
 
 bot.dialog('/', function(session) {
     session.send('Hello World');
 });
-
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:3000/");
