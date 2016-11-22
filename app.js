3c1624fed2b9650c5c46971975a7d08837c8d4a8
alexandre <aobellianne@phoceis.asia>
2016-11-22 14:27:39 +0800

    hello world

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 /**
  * Created by alexandre on 21/11/2016.
  */
-var restify = require('restify');
-var builder = require('botbuilder');
+//var restify = require('restify');
+//var builder = require('botbuilder');
+//
+//var server = restify.createServer();
+//server.listen(process.env.PORT || 3000, function (){
+//    console.log('%s listening to %s', server.name, server.url);
+//});
+//
+//var connector = new builder.ChatConnector({
+//    appId: '1acda086-2dbe-4d12-8690-a4b19a8985f2',
+//    appPassword: 'PJN3jkngKiucpUMgzaxkmFz'
+//});
+//
+//var bot = new builder.UniversalBot(connector);
+//
+//server.post('/api/message', connector.listen());
+//
+//bot.dialog('/', function(session) {
+//    session.send('Hello World');
+//});
 
-var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function (){
-    console.log('%s listening to %s', server.name, server.url);
-});
-
-var connector = new builder.ChatConnector({
-    appId: '',
-    appPassword: ''
-});
-
-var bot = new builder.UniversalBot(connector);
-
-server.post('/api/message', connector.listen());
-
-bot.dialog('/', function(session) {
-    session.send('Hello World');
-});
+console.log("Hello World");
