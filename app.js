065aaccfc4885c069e403751b16b853beae2068a
alexandre <aobellianne@phoceis.asia>
2016-12-05 15:08:19 +0800

    Test

--- app.js
+++ app.js
@@ -82,28 +82,29 @@ var bot = new builder.UniversalBot(wechatConnector);
 // Bot dialogs
 bot.dialog('/', [
     function (session) {
-        if (session.userData && session.userData.name) {
-            if (session.message.attachments &&
-                session.message.attachments.length > 0) {
-                var atm = session.message.attachments[0];
-                if (atm.contentType == connector.WechatAttachmentType.Image) {
-                    var msg = new builder.Message(session).attachments([atm]);
-                    session.send(msg);
-                }
-            }
-            session.send("How are you, " + session.userData.name);
-        } else {
-            builder.Prompts.text(session, "What's your name?");
-        }
+        session.send("All right, ");
+        // if (session.userData && session.userData.name) {
+        //     if (session.message.attachments &&
+        //         session.message.attachments.length > 0) {
+        //         var atm = session.message.attachments[0];
+        //         if (atm.contentType == connector.WechatAttachmentType.Image) {
+        //             var msg = new builder.Message(session).attachments([atm]);
+        //             session.send(msg);
+        //         }
+        //     }
+        //     session.send("How are you, " + session.userData.name);
+        // } else {
+        //     builder.Prompts.text(session, "What's your name?");
+        // }
     },
     function (session, results) {
-        session.userData.name = results.response;
-        session.send("OK, " + session.userData.name);
-        builder.Prompts.text(session, "What's your age?");
+        // session.userData.name = results.response;
+        // session.send("OK, " + session.userData.name);
+        // builder.Prompts.text(session, "What's your age?");
     },
     function (session, results) {
-        session.userData.age = results.response;
-        session.send("All right, " + results.response);
+        // session.userData.age = results.response;
+        // session.send("All right, " + results.response);
     }
 ]);
 
