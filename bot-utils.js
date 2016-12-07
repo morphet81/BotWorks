d9199d45fe92d6a0d7312936de04b2fc32979a1f
alexandre <aobellianne@phoceis.asia>
2016-12-07 16:16:01 +0800

    test

--- bot-utils.js
+++ bot-utils.js
@@ -1,10 +1,8 @@
 var exec = require('child_process').exec;
 
-module.exports = () => {
-    var module = {};
-
+module.exports = {
     // Convert a file with ffmpeg
-    module.ffmpegConvert = function (input, output, callback) {
+    ffmpegConvert: function (input, output, callback) {
         var isWin = /^win/.test(process.platform);
         var cmd = 'ffmpeg -y -i ' + input + ' ' + output;
 
@@ -16,7 +14,5 @@ module.exports = () => {
         exec(cmd, function(error, stdout, stderr) {
             callback(error, stdout, stderr, output);
         });
-    };
-
-    return module;
+    }
 };
