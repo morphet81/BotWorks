53016a40a83a6ce7c1b560a185e57925ee96998b
alexandre <aobellianne@phoceis.asia>
2016-12-09 16:46:47 +0800

    test

--- phoceis.js
+++ phoceis.js
@@ -62,12 +62,38 @@ module.exports = (wechatConnector) => {
         })
         .matches('Help', builder.DialogAction.send('phoceis_help'))
         .onDefault((session) => {
-            if(session.message.introduction) {
-                session.send('phoceis_dialog_intro');
-            }
-            else {
-                session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
-            }
+
+            return [
+                new builder.HeroCard(session)
+                    .title('Azure Storage')
+                    .subtitle('Massively scalable cloud storage for your applications')
+                    .text('Store and help protect your data. Get durable, highly available data storage across the globe and pay only for what you use.')
+                    .images([
+                        builder.CardImage.create(session, 'https://acom.azurecomcdn.net/80C57D/cdn/mediahandler/docarticles/dpsmedia-prod/azure.microsoft.com/en-us/documentation/articles/storage-introduction/20160801042915/storage-concepts.png')
+                    ])
+                    .buttons([
+                        builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/storage/', 'Learn More')
+                    ]),
+
+                new builder.ThumbnailCard(session)
+                    .title('DocumentDB')
+                    .subtitle('Blazing fast, planet-scale NoSQL')
+                    .text('NoSQL service for highly available, globally distributed apps—take full advantage of SQL and JavaScript over document and key-value data without the hassles of on-premises or virtual machine-based cloud database options.')
+                    .images([
+                        builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/29f4/beb4b953-ab91-4a31-b16a-71fb6d6829f4/WhatisAzureDocumentDB_960.jpg')
+                    ])
+                    .buttons([
+                        builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/documentdb/', 'Learn More')
+                    ])
+            ];
+
+
+            // if(session.message.introduction) {
+            //     session.send('phoceis_dialog_intro');
+            // }
+            // else {
+            //     session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
+            // }
         });
 
     return module;
