aff3e475e6c32b25148d9f9fb3c4cfbe1949fb94
alexandre <aobellianne@phoceis.asia>
2016-12-12 15:30:14 +0800

    tests

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require('dotenv-extended').load();
 
 var express         = require('express'),
     builder         = require('botbuilder'),
-    connector       = require('botbuilder-wechat-connector'),
+    // connector       = require('botbuilder-wechat-connector'),
+    connector       = require('./wechat'),
     util            = require('util'),
     fs              = require('fs'),
     botUser         = require('./user');
