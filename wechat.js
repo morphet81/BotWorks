8efb1dc1f3dcb0d523f92656fb62dbaf950a9372
alexandre <aobellianne@phoceis.asia>
2016-12-12 15:58:13 +0800

    tests

--- wechat.js
+++ wechat.js
@@ -47,7 +47,7 @@ var WechatConnector = (function() {
                 self.processMessage(wechatMessage);
                 // res.contentType = "text/xml";
                 // res.content = "<xml><ToUserName><![CDATA[oHtPEwBmVoxhvl3bw5WLan8s-XZw]]></ToUserName><FromUserName><![CDATA[gh_4bce4ef52d79]]></FromUserName><CreateTime>12345678</CreateTime><MsgType><![CDATA[text]]></MsgType> <Content><![CDATA[Hello]]></Content></xml>";
-                res.status(200).end();
+                res.content('test').status(200).end();
 
                 console.log(util.inspect(wechatMessage));
                 console.log(util.inspect(res));
