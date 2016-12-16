4ff3eb16a72ac5df670d2f3c04f179948c94303f
alexandre <aobellianne@phoceis.asia>
2016-12-16 17:16:22 +0800

    test

--- src/index.js
+++ src/index.js
@@ -73,7 +73,7 @@ module.exports = {
                 wechatUtils.getUserAccessToken(authCode)
                     .then(function (response) {
                         // Create the order on Wechat side
-                        wechatUtils.createUnifiedOrder(req, 'Trip to Bali', randomstring.generate(), 1, `${process.env.WECHAT_PAYMENT_CALLBACK_PAGE}`, `bali_trip_demo`, response.openid)
+                        wechatUtils.createUnifiedOrder(req, 'Trip to Bali', randomstring.generate(), 1, `${process.env.WECHAT_PAYMENT_CALLBACK_PAGE}?order_id=alsdkfalsdjfd`, `bali_trip_demo`, response.openid)
                             .then(function(prepaidConfig) {
                                 // Get config params for using wechat JS API
                                 wechatUtils.getJsapiConfig(req)
