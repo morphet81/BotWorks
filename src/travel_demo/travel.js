5764de2a832b5b4bcb26b65d15ef377e0ce78a7d
alexandre <aobellianne@phoceis.asia>
2016-12-19 17:31:46 +0800

    test

--- src/travel_demo/travel.js
+++ src/travel_demo/travel.js
@@ -112,12 +112,9 @@ module.exports = (wechatConnector) => {
         },
         function(session) {
             builder.Prompts.text(session, 'holiday_hotel_payment');
-        },
-        function(session) {
-            session.send('holiday_hotel_payment_validation');
 
-            var showValidation = setInterval(function () {
-                clearInterval(showValidation);
+            setTimeout(function () {
+                console.log('asdlkfa;sdlfjka;lsdfj');
                 session.send('holiday_hotel_payment_validation');
             }, 2000);
         }
