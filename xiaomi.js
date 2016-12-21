152cf3891f65dad401e06e2090ac1bf4e4142a26
alexandre <aobellianne@phoceis.asia>
2016-12-21 16:14:27 +0800

    test

---
+++ xiaomi.js
@@ -0,0 +1,35 @@
+/**
+ * Created by alexandre on 21/12/2016.
+ */
+jQuery.ajaxSetup({
+    dataFilter: function (data, type) {
+        console.log(data);
+        return data;
+    }
+});
+
+var i = 0;
+var test = function() {
+    // Click the button
+    $("#J_flashBtn").trigger("click");
+
+    if(i % 3 == 0) {
+        console.log('Check clicking : ' + i);
+    }
+
+    if($("#J_miAlert").is(':visible') || $("#J_modalFlashSoldout").is(':visible') || $("#J_modalAward").is(':visible') || $("#J_modalVideo").is(':visible')) {
+        console.log('close dialogs');
+        $("#J_modalFlashSoldout a").trigger('click');
+        $("#J_miAlertConfirm").trigger('click');
+    }
+
+    if(!$("#J_flashBtn").hasClass('btn-disable')) {
+        setTimeout(function () {
+            console.log('Stop trying')
+            clearInterval(autoClick);
+        }, 10000);
+    }
+    i++;
+};
+
+var autoClick = setInterval(test, 400);
